c71ef40dc5efcefcf13144c0232d3a354d926140
MuratYucedag <yucedagmurat23@gmail.com>
2023-05-30 10:39:47 +0300

    Ders 20 sonu

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class MyAccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
